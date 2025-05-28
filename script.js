82d054bd2bcdd6afd40fdc13efb3afbc90795202
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-05-28 22:18:54 +0530

    Added css properties to the different modes and started working on javascript file

---
+++ script.js
@@ -0,0 +1,22 @@
+// Quiz App JavaScript Code
+
+
+// quiz start btn  declaration
+let quizStartBtn = document.getElementsByClassName("startbtn");
+
+
+//  quiz section difficulty levels cards
+let easyMode = document.getElementsByClassName("difficulty-easy-card");
+let mediumMode = document.getElementsByClassName("difficulty-medium-card");
+let hardMode = document.getElementsByClassName("difficulty-hard-card");
+
+
+
+
+
+
+
+
+let quizstart = ()  => {
+
+}
