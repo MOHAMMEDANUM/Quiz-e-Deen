62418d6e701253c1b43ea39349231efaad292bfe
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-06-06 23:24:20 +0530

    Added questions,option and functionality Hard Mode

--- hardModeScript.js
+++ hardModeScript.js
@@ -34,3 +34,52 @@ nextGameButton = document.getElementById("next-game-btn")
 
 // Previous Question Button
 previousGameButton = document.getElementById("previous-game-btn")
+
+
+
+
+// 
+
+const hardModeQuestions = [
+
+    {
+        question : "who are you?",
+        answer : [
+            {text : "anum",  correct :true},
+            {text : "saad",  notcorrect :false},
+            {text : "owaies", notcorrect :false},
+            {text : "saud",  notcorrect :false}
+        ]
+    },
+
+    {
+        question : "who is your brother?",
+        answer : [
+            {text : "anum",  correct :false},
+            {text : "abrar",  correct :false},
+            {text : "owaies",  correct :true},
+            {text : "saud",  correct :false}
+        ]
+
+    },
+
+    {
+        question : "which brother is saudi return?",
+        answer : [
+            {text : "anum",  correct :false},
+            {text : "saad",  correct :true},
+            {text : "owaies",  correct :false},
+            {text : "kaif",  correct :false}
+        ]
+    },
+
+    {
+        question : "what?",
+        answer : [
+            {text : "anum",  correct :false},
+            {text : "saad",  correct :true},
+            {text : "owaies",  correct :false},
+            {text : "kaif",  correct :false}
+        ]
+    }
+]
