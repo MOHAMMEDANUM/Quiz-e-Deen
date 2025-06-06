82dba4889b80b93e00209ff720f2272e62c922cf
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-06-06 23:21:39 +0530

    Added Hard Mode Properties

---
+++ hardModeScript.js
@@ -0,0 +1,36 @@
+                            //                hard Mode
+
+
+// Hard Mode Properties
+    
+// Question counter selector
+
+questionCounter=document.getElementById("question-counter")
+
+// Question Number selector
+
+questionNumber=document.getElementById("ques-no")
+
+// Progress percentage selector
+progressPercentage=document.getElementById("progress-percentage")
+
+// progress fill selector
+progressFill=document.getElementById("progress-fill")
+
+// timer display selector
+timerDisplay=document.getElementById("timer-display")
+
+
+// question full body section
+questionSection=document.getElementById("question-section")
+
+// time-out-msg
+timeOutMsg=document.getElementById("time-out-msg")
+
+
+
+// Next Question Button
+nextGameButton = document.getElementById("next-game-btn")
+
+// Previous Question Button
+previousGameButton = document.getElementById("previous-game-btn")
