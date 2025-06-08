1e31a38d25a53bfa9c2f7c11f35248bba898552c
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-06-08 09:29:28 +0530

    Modified the javascript code accordlingly for options-container in easy mode

--- script.js
+++ script.js
@@ -109,9 +109,7 @@ let optionLetterA = document.querySelector("#option-letter-A")
 let optionLetterB = document.querySelector("#option-letter-B")
 let optionLetterC = document.querySelector("#option-letter-C")
 let optionLetterD = document.querySelector("#option-letter-D")
-
-// 
-let optionLetter=document.querySelector(".option-letter")
+// console.log(optionLetterD);
 
 // Question counter selector
 
@@ -146,23 +144,16 @@ let nextGameButton = document.getElementById("next-game-btn")
 let previousGameButton = document.getElementById("previous-game-btn")
 
 
-// Selecting option function
-
-let optionDiv = document.getElementsByClassName("option")
-let optionletterA =document.getElementById("option-letter-A")
-// console.log(optionDiv);
-
-// console.log(optionletterA.innerHTML);
-
-
-
 //  Question of easy mode
 
 let easyModeQuestion = [
-
+,"Zakat, Sawm, Salat, Hajj, Shahada","Salat, Sawm, Shahada, Zakat, Hajj","Hajj, Sawm, Zakat, Shahada, Salat"
     {
         question : "What are the five pillars of Islam?",
-        options : ["Shahada, Zakat, Hajj, Sawm, Salat","Zakat, Sawm, Salat, Hajj, Shahada","Salat, Sawm, Shahada, Zakat, Hajj","Hajj, Sawm, Zakat, Shahada, Salat"],
+        answers : [
+                    {text :"Shahada, Zakat, Hajj, Sawm, Salat"},{correctOption : }
+
+                  ],
         answer : "Shahada, Zakat, Hajj, Sawm, Salat"
     },
 
@@ -371,113 +362,3 @@ let optionSelecting = (e)=>{
     
     // optionDiv[0].style="background-color:limegreen;color:white" 
 }
-
-
-                            //                Medium Mode
-
-// Medium Mode Properties
-    
-// Question counter selector
-
-questionCounter=document.getElementById("question-counter")
-
-// Question Number selector
-
-questionNumber=document.getElementById("ques-no")
-
-// Progress percentage selector
-progressPercentage=document.getElementById("progress-percentage")
-
-// progress fill selector
-progressFill=document.getElementById("progress-fill")
-
-// timer display selector
-timerDisplay=document.getElementById("timer-display")
-
-
-// question full body section
-questionSection=document.getElementById("question-section")
-
-// time-out-msg
-timeOutMsg=document.getElementById("time-out-msg")
-
-
-
-// Next Question Button
-nextGameButton = document.getElementById("next-game-btn")
-
-// Previous Question Button
-previousGameButton = document.getElementById("previous-game-btn")
-
-
-
-const mediumModeQuestions = [
-
-    {
-        question : "who are you?",
-        answer : [
-            {text : "anum",  correct :true},
-            {text : "saad",  correct :false},
-            {text : "owaies",  correct :false},
-            {text : "saud",  correct :false}
-        ]
-    },
-
-    {
-        question : "who is your brother?",
-        answer : [
-            {text : "anum",  correct :false},
-            {text : "saad",  correct :false},
-            {text : "owaies",  correct :true},
-            {text : "saud",  correct :false}
-        ]
-
-    },
-
-    {
-        question : "which brother is saudi return?",
-        answer : [
-            {text : "anum",  correct :false},
-            {text : "saad",  correct :true},
-            {text : "owaies",  correct :false},
-            {text : "saud",  correct :false}
-        ]
-    }
-]
-
-// Medium Mode Question Tag
-let mediumQuestionText=document.getElementById("medium-question-text")
-
-// Medium Mode Options Tag
-let options=document.getElementsByClassName("options-Medium-Mode")
-
-//  currentQuestion
-let currentQuestion = 0
-
-//  score
-let score = 0
-
-// start-game-medium-btn
-let startGameMediumBtn=document.getElementById("start-game-medium-btn")
-
-
-// Showing the Question function
-let showQuestion = ()=>{
-    currentQuestion = 0
-    score = 0
-    mediumQuestionText.innerText=mediumModeQuestions[currentQuestion].question
-
-    for(let i=0;i<4;i++){
-        options[i].innerHTML=mediumModeQuestions[currentQuestion].answer[i].text
-        // console.log(options[i]);
-        
-    }
-    
-}
-startGameMediumBtn.addEventListener("click",showQuestion)
-
-// Showing the Options function
-let ShowOptions = ()=>{
-
-}
-
